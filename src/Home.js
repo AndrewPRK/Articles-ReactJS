0a57086b949880540cf670a6f78de022f1c4b307
Andrew <Andyhaprk@gmail.com>
2018-02-11 13:29:45 +0200

    edit home

--- src/Home.js
+++ src/Home.js
@@ -2,7 +2,7 @@ import React from "react"
 export default class extends React.Component {
 render(){
     return (
-        <div>
+        <div className="page-home">
             <img src = "../img/articlesWriting.jpg"/>
         </div>
     );
