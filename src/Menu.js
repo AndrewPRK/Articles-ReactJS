c36f42cec4791dd8968d4b8116995b3b455f5e5d
Andrew <Andyhaprk@gmail.com>
2018-02-11 13:45:56 +0200

    edit article loader style

--- src/Menu.js
+++ src/Menu.js
@@ -4,8 +4,7 @@ export default (props) => {
     return (
         <div className="main-menu">
             <Link className="button-menu" to="/"><h2>Menu</h2></Link>
-            {/* <div><NavLink to = "/filters" activeStyle={{color:"red"}}>Filters</NavLink></div> */}
-            <div><NavLink className="nav-butt-menu" to="/articles" activeStyle={{ color: "blue" }}>Articles</NavLink></div>
+            <NavLink className="nav-butt-menu" to="/articles" activeStyle={{ color: "blue" }}>Articles</NavLink>
         </div>
     )
 }
